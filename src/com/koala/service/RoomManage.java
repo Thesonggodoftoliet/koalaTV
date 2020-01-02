3d6433a67fdb1ae6c6bd00f60f2031b4aacf3eb9
Thesonggodoftoliet <535644752@qq.com>
2020-01-02 17:28:02 +0800

    完成申请直播接口

--- src/com/koala/service/RoomManage.java
+++ src/com/koala/service/RoomManage.java
@@ -2,6 +2,8 @@ package com.koala.service;
 
 import com.koala.entity.room_tb;
 
+import java.util.List;
+
 /**
  * 对直播间的管理.
  * @author Marting.Lee
@@ -10,4 +12,5 @@ import com.koala.entity.room_tb;
 public interface RoomManage {
     int addRoom(room_tb room);
     String onLiving(int userid,String title);
+    List<room_tb> getRoomsOnlive();
 }
